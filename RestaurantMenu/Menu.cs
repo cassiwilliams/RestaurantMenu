56329f8e51480e09f04a3aaa4db101fdf843c8df
cassiwilliams <cassiwelter@hotmail.com>
2020-06-05 20:04:56 -0400

    "Pushing RestaurantMenu to remote repository"

--- RestaurantMenu/Menu.cs
+++ RestaurantMenu/Menu.cs
@@ -9,5 +9,12 @@ namespace RestaurantMenu
         /*
          * Menu should include current date, display items, and ability to input new items and pass it to the menu items class list/dictionary
          */
+        public List<MenuItem> MenuItems { get; set; }
+        public DateTime MenuDateTime { get; }
+        public Menu(List<MenuItem> item, DateTime updateDate)
+        {
+            MenuItems = item;
+            MenuDateTime = updateDate;
+        }
     }
 }
--- RestaurantMenu/MenuItem.cs
+++ RestaurantMenu/MenuItem.cs
@@ -9,5 +9,17 @@ namespace RestaurantMenu
         /*
          * MenuItems should be the class holding all the items price, description, and category, including the date (to match with date last modified), privately, and the date it was last modified
          */
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsNew { get; set; }
+
+        public MenuItem(float price, string description, string category, bool isNew)
+        {
+            Price = price;
+            Description = description;
+            Category = category;
+            IsNew = isNew;
+        }
     }
 }
